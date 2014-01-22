6646afa8c9c2dcf06c7bd1728cdf1f76574ec9e8
Timothy Ong <tim.ong.118@gmail.com>
2014-01-22 09:25:04 -0500

    made it easier

--- init.py
+++ init.py
@@ -17,70 +17,31 @@ mobos = data[13]
 psus = data[16]
 ssds = data[22]
 gpus = data[23]
-catID = str(gpus["CategoryID"])
-nodID = str(gpus["NodeId"])
+group = [cases,cpus,hdds,ram,mobos,psus,ssds,gpus]
+names = ["cases","cpus","hdds","ram","mobos","psus","ssds","gpus"]
 
-url = "http://www.ows.newegg.com/Stores.egg/Navigation/1/{Cat}/{Node}"
-url1 = url.replace("{Cat}", catID)
-url2 = url1.replace("{Node}", nodID)
-resp = urllib2.urlopen(url2)
-data = json.loads(resp.read())
-#data[1] for motherboards to account for intel and amd
-subcatID = data[0]["CategoryID"]
-nodeID = data[0]["NodeId"]
+i = 0
+while (i < 8):
+    catID = str(group[i]["CategoryID"])
+    nodID = str(group[i]["NodeId"])
+    
+    url = "http://www.ows.newegg.com/Stores.egg/Navigation/1/{Cat}/{Node}"
+    url1 = url.replace("{Cat}", catID)
+    url2 = url1.replace("{Node}", nodID)
+    resp = urllib2.urlopen(url2)
+    data = json.loads(resp.read())
+    #data[1] for motherboards to account for intel and amd
+    subcatID = data[0]["CategoryID"]
+    nodeID = data[0]["NodeId"]
+    
 
-
-data = {
-    "SubCategoryId": subcatID, 
-    "NValue": "", 
-    "StoreDepaId": 1, 
-    "NodeId": nodeID, 
-    "BrandId": -1, 
-    "PageNumber": 1, 
-    "CategoryId": catID
-}
-
-url = "http://www.ows.newegg.com/Search.egg/Advanced"
-params = json.dumps(data).replace("null","-1")
-request = urllib2.Request(url, params)
-response = urllib2.urlopen(request)
-data = json.loads(response.read())
-count = 0
-asdf = data["ProductListItems"]
-
-
-while (count < 20):
-    final = asdf[count]
-    addstuff = dict()
-    addstuff["Title"] = final["Title"].encode("ascii","ignore")
-    addstuff["fPrice"] = final["FinalPrice"].encode("ascii","ignore")
-    addstuff["oPrice"] = final["OriginalPrice"].encode("ascii","ignore")
-    addstuff["Model"] = final["Model"].encode("ascii","ignore")
-    addstuff["ItemNumber"] = final["ItemNumber"].encode("ascii","ignore")
-    addstuff["Newegg"] = final["NeweggItemNumber"].encode("ascii","ignore")
-    addstuff["Rating"] = final["ReviewSummary"]["Rating"]
-    addstuff["numRating"] = final["ReviewSummary"]["TotalReviews"].encode("ascii","ignore")
-    db.gpus.insert({'itemnumber':addstuff['ItemNumber'], 'stuff':addstuff})
-    print(addstuff["Title"])
-    count = count + 1
-
-#url = "http://www.ows.newegg.com/Products.egg/{item}/Specification".replace("{item}", addstuff["ItemNumber"])
-#response = urllib2.urlopen(request)
-#data = json.loads(response.read())
-#print data
-
-#print data["ProductListItems"][0]
-total = data["PaginationInfo"]["TotalCount"]
-
-pagenum = 2
-while (pagenum * 20 < total):
     data = {
-        "SubCategoryId": subcatID,
-        "NValue": "",
-        "StoreDepaId": 1,
-        "NodeId": nodeID,
-        "BrandId": -1,
-        "PageNumber": pagenum,
+        "SubCategoryId": subcatID, 
+        "NValue": "", 
+        "StoreDepaId": 1, 
+        "NodeId": nodeID, 
+        "BrandId": -1, 
+        "PageNumber": 1, 
         "CategoryId": catID
         }
     
@@ -104,9 +65,54 @@ while (pagenum * 20 < total):
         addstuff["Newegg"] = final["NeweggItemNumber"].encode("ascii","ignore")
         addstuff["Rating"] = final["ReviewSummary"]["Rating"]
         addstuff["numRating"] = final["ReviewSummary"]["TotalReviews"].encode("ascii","ignore")
-        db.gpus.insert({'itemnumber':addstuff['ItemNumber'], 'stuff':addstuff})
+        db.names[i].insert({'itemnumber':addstuff['ItemNumber'], 'stuff':addstuff})
         print(addstuff["Title"])
         count = count + 1
+        
+#url = "http://www.ows.newegg.com/Products.egg/{item}/Specification".replace("{item}", addstuff["ItemNumber"])
+#response = urllib2.urlopen(request)
+#data = json.loads(response.read())
+#print data
 
-    pagenum = pagenum + 1
+#print data["ProductListItems"][0]
+    total = data["PaginationInfo"]["TotalCount"]
+        
+    pagenum = 2
+    while (pagenum * 20 < total):
+        data = {
+            "SubCategoryId": subcatID,
+            "NValue": "",
+            "StoreDepaId": 1,
+            "NodeId": nodeID,
+            "BrandId": -1,
+            "PageNumber": pagenum,
+            "CategoryId": catID
+            }
+        
+        url = "http://www.ows.newegg.com/Search.egg/Advanced"
+        params = json.dumps(data).replace("null","-1")
+        request = urllib2.Request(url, params)
+        response = urllib2.urlopen(request)
+        data = json.loads(response.read())
+        count = 0
+        asdf = data["ProductListItems"]
+        
+        
+        while (count < 20):
+            final = asdf[count]
+            addstuff = dict()
+            addstuff["Title"] = final["Title"].encode("ascii","ignore")
+            addstuff["fPrice"] = final["FinalPrice"].encode("ascii","ignore")
+            addstuff["oPrice"] = final["OriginalPrice"].encode("ascii","ignore")
+            addstuff["Model"] = final["Model"].encode("ascii","ignore")
+            addstuff["ItemNumber"] = final["ItemNumber"].encode("ascii","ignore")
+            addstuff["Newegg"] = final["NeweggItemNumber"].encode("ascii","ignore")
+            addstuff["Rating"] = final["ReviewSummary"]["Rating"]
+            addstuff["numRating"] = final["ReviewSummary"]["TotalReviews"].encode("ascii","ignore")
+            db.names[i].insert({'itemnumber':addstuff['ItemNumber'], 'stuff':addstuff})
+            print(addstuff["Title"])
+            count = count + 1
+            
+        pagenum = pagenum + 1
 
+    i = i + 1
