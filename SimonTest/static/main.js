3e84ba6e84d523b30e6ac42063ea5fb2d2c46568
Siimon13 <chen.simon89@gmail.com>
2014-01-27 03:33:37 -0500

    Added Filtering js

---
+++ SimonTest/static/main.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+$( document ).ready(function() {
+    $( "a" ).click(function( event ) {
+	alert( "The link will no longer take you to jquery.com" );
+	event.preventDefault();
+    });
+});
