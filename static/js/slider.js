0f3ce5bc7b13f99363f6f1f6766dbd8875b2d7b3
RatedRion <ratedrion@gmail.com>
2014-01-28 08:49:45 -0500

    yes

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,23 +1,12 @@
 $(function() {
 		$( "#slider" ).slider({
-			value:100,
-			min: 0,
-			max: 500,
-			step: 100,
+			value:300,
+			min: 300,
+			max: 700,
+			step: 10,
 			slide: function( event, ui ) {
 				$( "#amount" ).val( "$" + ui.value );
 			}
 		});
 		$( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
-
-		$( "#slider2" ).slider({
-			value:100,
-			min: 0,
-			max: 500,
-			step: 100,
-			slide: function( event, ui ) {
-				$( "#amount2" ).val( "$" + ui.value );
-			}
-		});
-		$( "#amount2" ).val( "$" + $( "#slider2" ).slider( "value" ) );
 	});
