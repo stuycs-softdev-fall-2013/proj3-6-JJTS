e43a347a8d7f011e75bd74a02853145aa5b4d2cd
RatedRion <ratedrion@gmail.com>
2014-01-28 09:55:55 -0500

    doesn't work after 1 week of code...

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,12 +1,19 @@
 $(function() {
-		$( "#slider" ).slider({
-			value:300,
-			min: 300,
-			max: 700,
-			step: 10,
-			slide: function( event, ui ) {
-				$( "#amount" ).val( "$" + ui.value );
-			}
-		});
-		$( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+	$( "#slider" ).slider({
+		value:300,
+		min: 300,
+		max: 700,
+		step: 10,
+		slide: function( event, ui ) {
+			$( "#amount" ).val( "$" + ui.value );
+		}
 	});
+	$( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+});
+
+$(function build() {
+	if ($( "#slider" ).slider( "value" ) < 400) {
+		document.getElementById("build").innerHTML='<a href="/add/19-113-335">AMD A6-6400K 3.9GHz Dual-Core Processor</a>';
+	}
+	setTimeout(build,1000);
+});
